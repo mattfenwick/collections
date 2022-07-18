dc89586ac88a39361c2c5ed0c8f985c13c1f7ccc
Matthew Fenwick <mfenwick100@gmail.com>
2022-07-18 09:24:09 -0400

    get map comparison, equality standardized

--- pkg/maps/compare.go
+++ pkg/maps/compare.go
@@ -3,104 +3,9 @@ package maps
 import (
 	. "github.com/mattfenwick/collections/pkg/base"
 	"github.com/mattfenwick/collections/pkg/builtins"
+	"github.com/mattfenwick/collections/pkg/slices"
 	"golang.org/x/exp/constraints"
 )
-
-/*
-TODO
-func EqualMapHelper[A any](compare F2[A, A, bool], xs []A, ys []A) bool {
-	// unfortunately, can't do:
-	//   return Equal(xs, ys)
-	//   because: A does not implement comparable
-	if len(xs) != len(ys) {
-		return false
-	}
-	for i := range xs {
-		if !compare(xs[i], ys[i]) {
-			return false
-		}
-	}
-	return true
-}
-
-func EqualMap[A any](compare F2[A, A, bool]) F2[[]A, []A, bool] {
-	return func(xs []A, ys []A) bool {
-		return EqualSliceHelper(compare, xs, ys)
-	}
-}
-
-func EqualPair[A Eq[A], B Eq[B]](p1 *Pair[A, B], p2 *Pair[A, B]) bool {
-	return EqualBy[*Pair[A, B]](
-		functions.On(Equal[A], First[A, B]),
-		functions.On(Equal[B], Second[A, B]))(p1, p2)
-}
-
-func EqualBy[A any](comparisons ...Equaler[A]) Equaler[A] {
-	return EqualBys(comparisons)
-}
-
-func EqualBys[A any](comparisons []Equaler[A]) Equaler[A] {
-	return func(x A, y A) bool {
-		return All(func(c Equaler[A]) bool {
-			return c(x, y)
-		}, comparisons)
-	}
-}
-
-// CompareSliceHelper should work as in Haskell.  Examples from Haskell:
-//   Prelude> [1,2,3] < [3,4,5]
-//   True
-//   Prelude> [1,2,3] < [3,4]
-//   True
-//   Prelude> [1,2,3] < []
-//   False
-func CompareSliceHelper[A any](compare Comparator[A], xs []A, ys []A) Ordering {
-	i := 0
-	for {
-		if i == len(xs) && i == len(ys) {
-			return OrderingEqual
-		} else if i == len(xs) {
-			return OrderingLessThan
-		} else if i == len(ys) {
-			return OrderingGreaterThan
-		}
-		comp := compare(xs[i], ys[i])
-		if comp != OrderingEqual {
-			return comp
-		}
-		i++
-	}
-}
-func CompareSlice[A any](compare Comparator[A]) Comparator[[]A] {
-	return func(xs []A, ys []A) Ordering {
-		return CompareSliceHelper(compare, xs, ys)
-	}
-}
-
-func ComparePair[A Ord[A], B Ord[B]](p1 *Pair[A, B], p2 *Pair[A, B]) Ordering {
-	return CompareBy[*Pair[A, B]](
-		functions.On(Compare[A], First[A, B]),
-		functions.On(Compare[B], Second[A, B]))(p1, p2)
-}
-
-func CompareBy[A any](comparisons ...Comparator[A]) Comparator[A] {
-	return CompareBys(comparisons)
-}
-
-func CompareBys[A any](comparisons []Comparator[A]) Comparator[A] {
-	return func(x A, y A) Ordering {
-		ords := Map(func(c Comparator[A]) Ordering {
-			return c(x, y)
-		}, comparisons)
-		for _, o := range ords {
-			if o != OrderingEqual {
-				return o
-			}
-		}
-		return OrderingEqual
-	}
-}
-*/
 
 func CompareMapIndexOrd[A comparable, B Ord[B]](key A) Comparator[map[A]B] {
 	return CompareMapIndexBy(key, Compare[B])
@@ -126,5 +31,25 @@ func CompareMapIndexBy[A comparable, B any](key A, compare Comparator[B]) Compar
 			// key not found in y
 			return OrderingLessThan
 		}
+	}
+}
+
+func CompareMapPairwiseOrd[A constraints.Ordered, B Ord[B]]() Comparator[map[A]B] {
+	return CompareMapPairwiseBy[A, B](Compare[B])
+}
+
+func CompareMapPairwiseOrdered[A constraints.Ordered, B constraints.Ordered]() Comparator[map[A]B] {
+	return CompareMapPairwiseBy[A, B](builtins.CompareOrdered[B])
+}
+
+// CompareMapPairwiseBy works by project a map to a list, therefore it's inefficient and probably
+//   best to avoid using unless absolutely necessary!
+//   Note: while `map` requires `A` to be `comparable`, *comparing* maps requires `A` to be Ordered
+//   as well!
+func CompareMapPairwiseBy[A constraints.Ordered, B any](compare Comparator[B]) Comparator[map[A]B] {
+	comparePair := slices.ComparePairBy(builtins.CompareOrdered[A], compare)
+	compareSlice := slices.CompareSlicePairwiseBy(comparePair)
+	return func(xs map[A]B, ys map[A]B) Ordering {
+		return compareSlice(ToSlice(xs), ToSlice(ys))
 	}
 }
---
+++ pkg/maps/equal.go
@@ -0,0 +1,50 @@
+package maps
+
+import (
+	. "github.com/mattfenwick/collections/pkg/base"
+	"github.com/mattfenwick/collections/pkg/builtins"
+	"github.com/mattfenwick/collections/pkg/slices"
+)
+
+func EqualMapIndexEq[A comparable, B Eq[B]](key A) Equaler[map[A]B] {
+	return EqualMapIndexBy(key, Equal[B])
+}
+
+func EqualMapIndexComparable[A comparable, B comparable](key A) Equaler[map[A]B] {
+	return EqualMapIndexBy(key, builtins.Equal[B])
+}
+
+// EqualMapIndexBy equals a single index
+func EqualMapIndexBy[A comparable, B any](key A, equal Equaler[B]) Equaler[map[A]B] {
+	return func(xs map[A]B, ys map[A]B) bool {
+		x, xok := xs[key]
+		y, yok := ys[key]
+		if xok && yok {
+			// both have the key?  check the value's equality
+			return equal(x, y)
+		} else if !xok && !yok {
+			// neither has it
+			return true
+		}
+		// one has it and the other doesn't
+		return false
+	}
+}
+
+func EqualMapPairwiseEq[A comparable, B Eq[B]]() Equaler[map[A]B] {
+	return EqualMapPairwiseBy[A, B](Equal[B])
+}
+
+func EqualMapPairwiseComparable[A comparable, B comparable]() Equaler[map[A]B] {
+	return EqualMapPairwiseBy[A, B](builtins.Equal[B])
+}
+
+// EqualMapPairwiseBy works by project a map to a list, therefore it's inefficient and probably
+//   best to avoid using unless absolutely necessary!
+func EqualMapPairwiseBy[A comparable, B any](equal Equaler[B]) Equaler[map[A]B] {
+	equalPair := slices.EqualPairBy(builtins.Equal[A], equal)
+	equalSlice := slices.EqualSlicePairwiseBy(equalPair)
+	return func(xs map[A]B, ys map[A]B) bool {
+		return equalSlice(ToSlice(xs), ToSlice(ys))
+	}
+}
